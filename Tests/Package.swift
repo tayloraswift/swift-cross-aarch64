c89ca0c370a5f70117c2261d4763ccb8d584a6d5
Diana Ma <diana@rarestype.com>
2024-12-06 00:48:26 +0000

    test cross-compilation

---
+++ Tests/Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package:Package = .init(
+    name: "swift-cross-compile-test",
+    products: [
+        .executable( name: "HelloBarbie", targets: ["HelloBarbie"]),
+    ],
+    targets: [
+        .executableTarget(name: "HelloBarbie"),
+    ])
