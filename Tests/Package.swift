45e1d5ab8d3fb6c53b8873b57c729e10ae300c74
Diana Ma <diana@rarestype.com>
2026-02-03 01:34:13 +0000

    reformat

--- Tests/Package.swift
+++ Tests/Package.swift
@@ -1,11 +1,12 @@
 // swift-tools-version: 6.0
 import PackageDescription
 
-let package:Package = .init(
+let package: Package = .init(
     name: "swift-cross-compile-test",
     products: [
         .executable( name: "HelloBarbie", targets: ["HelloBarbie"]),
     ],
     targets: [
         .executableTarget(name: "HelloBarbie"),
-    ])
+    ]
+)
